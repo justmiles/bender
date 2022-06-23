802453052c76ff741601a462cc8162ebfee02604
Miles Maddox <j.milesmaddox@gmail.com>
2022-06-23 01:48:06 +0000

    roast and insult

---
+++ scripts/roast.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# Usage:
+#  roast.sh justMiles
+
+cat <<EOF | shuf -n 1 $FILE | sed "s/USERNAME/$1/g"
+USERNAME, you're the reason there's instructions on shampoo bottles
+USERNAME, you're about as useful as an ashtray on a motorcycle
+USERNAME, I envy everyone that hasn't met you.
+USERNAME, words can't describe your beauty... but numbers can. 2/10
+USERNAME, you're like Rapunzel, but instead of letting down your hair, you just let down everyone in your life.
+USERNAME, if you were on life support I would pull the plug to charge my phone.
+USERNAME, this is why people talk about you.
+USERNAME, first of all, brush your teeth...
+USERNAME, you're not pretty enough to be this stupid.
+USERNAME, I'd like to see something from your point of view, but I can't seem to get my head that far up my ass.
+USERNAME, you are impossible to underestimate
+USERNAME, why are you playing so hard to get when you're already hard to want
+USERNAME, you're the reason the gene pool needs a lifeguard.
+USERNAME, you are neither rich nor hot enough to have this shit of a personality.
+USERNAME, somewhere out there is a tree, tirelessly producing oxygen so you can breathe. I think you owe it an apology.
+If I had a gun with two bullets and I was in a room with Hitler, Bin Laden, and USERNAME, I would shoot USERNAME twice.
+USERNAME, your gene pool could use some chlorine in it.
+USERNAME, I expected nothing from you, and I'm still disappointed.
+USERNAME, I don't care what people say about you, I think you're alright.
+USERNAME, You do realize that people just tolerate you?
+I can explain it to you, USERNAME but I can't understand it for you.
+USERNAME, I don't care what people say about you or your eyebrows. You're alright.
+USERNAME, I would agree with you but then we'd both be wrong.
+USERNAME, 2090 called. You're dead and you wasted your time on earth.
+USERNAME, you must hear the phrase "let's just be friends" often.
+USERNAME, you look like your pacifier was made of asbestos.
+USERNAME, you look like you recently got set free from a block of ice.
+USERNAME, I'd like to see you and Reese Witherspoon in a chin fight.
+USERNAME, your hair color really brings out the pain and shame in your eyes.
+USERNAME, I bet you like your steak well done.
+USERNAME, you didn't turn out to be the person Mr. Rogers knew you could be.
+USERNAME, I love how you state the obvious with such a sense of discovery.
+USERNAME, you're just a cotton-headed ninny muggins.
+USERNAME, I envy everyone you have never met.
+USERNAME, You're like the end pieces of a loaf of bread. Everyone touches you, but nobody wants you.
+I would love to insult USERNAME but I'm afraid I won't do it as well as nature did.
+USERNAME, Are you naturally this dumb or do you have to put in effort?
+USERNAME, You consistently set low expectations and fail to achieve them.
+Just quit being yourself, USERNAME.
+USERNAME, If I wanted to kill myself, I'd climb up your ego, and jump to your IQ.
+I refuse to enter a battle of wits with an unarmed opponent.
+USERNAME could be the poster child for birth control.
+EOF
